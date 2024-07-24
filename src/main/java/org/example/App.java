99219c76289b28f27405b2572dc1ba051920deed
Mario Martins <mario.martins@analyst1.com>
2024-07-24 10:13:28 -0700

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+
+            page.addInitScript("const definition = 'Some script'");
+
+            page.navigate("http://playwright.dev");
+
+            page.evaluate("console.log(definition)");
+
+            System.out.println(page.title());
+        }
+    }
+}
